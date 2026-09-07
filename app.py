f49a9984c3fd62f9ff5209ec096f48d362633901
Mohammad Fauzan <mohammadfauzan0207@gmail.com>
2026-09-08 06:49:09 +0700

    feat: merilis teks sambutan baru via CI/CD

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import redis
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from prometheus_client import start_http_server, Counter
 
-# Menghubungkan ke container Redis
+# Membuat sensor penghitung (counter) untuk metrik Prometheus
+REQUEST_COUNT = Counter('app_requests_total', 'Total permintaan ke aplikasi web Python')
+
 cache = redis.Redis(host='redis-server', port=6379)
 
 class HaloHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # Menaikkan angka sensor setiap kali ada orang yang mengakses web
+        REQUEST_COUNT.inc()
+        
         try:
-            # Menambah dan mengambil angka dari Redis
             hits = cache.incr('hits')
-            pesan = f"Halo! Aplikasi terhubung ke Redis. Anda pengunjung ke-{hits}."
+            pesan = return f'🚀 Halo dari K3s CI/CD Pipeline! Anda pengunjung ke-{visits}.\n'
         except redis.exceptions.ConnectionError:
-            pesan = "Halo! (Gagal terhubung ke Redis, container mungkin mati)"
+            pesan = "Halo! (Gagal terhubung ke Redis)"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        # Mengirim respon ke browser/curl
         self.wfile.write(pesan.encode('utf-8'))
 
-server = HTTPServer(('0.0.0.0', 8080), HaloHandler)
-print("Server berjalan di port 8080...")
-server.serve_forever()
+if __name__ == '__main__':
+    # Membuka port 8000 khusus agar bisa disedot oleh Prometheus
+    start_http_server(8000)
+    
+    server = HTTPServer(('0.0.0.0', 8080), HaloHandler)
+    print("Server web di port 8080. Metrik Prometheus di port 8000...")
+    server.serve_forever()
+
